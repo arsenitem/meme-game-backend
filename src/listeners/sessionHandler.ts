6646d97c9b1ac4a824ede8ff35b7e62f77a7e004
arsenitem <arsenitem@bk.ru>
2022-04-11 17:35:04 +0300

    start session handler

--- src/listeners/sessionHandler.ts
+++ src/listeners/sessionHandler.ts
@@ -1,12 +1,14 @@
 import Session from "../models/session.model";
 import data from './../data/index';
-import {addSession, getPlayerById} from '../services/dataService';
+import {addSession, getPlayerById, addPlayerToSession, getSessionById} from '../services/dataService';
 export default (io: any, socket: any) => {
     const createSession = ({name}: {name: string}) => {
         const host = getPlayerById(socket.id)
         if (host) {
             const session = new Session(name, host);
             addSession(session);
+            addPlayerToSession(socket.id, session.id);
+            socket.emit("session:created", session);
         }
     }
 
@@ -16,8 +18,13 @@ export default (io: any, socket: any) => {
         }, 1000);
     }
 
-    const sessionStart = () => {
-        
+    const sessionStart = ({sessionId}: {sessionId: string}) => {
+        const session = getSessionById(sessionId);
+        setInterval(() => {
+            session?.players.forEach((player) => {
+                io.sockets.sockets.get(player.id).emit("session:status", session);
+            });
+        }, 1000);
     }
     socket.on('session:create', createSession);
     socket.on('session:getList', getSessionList);
